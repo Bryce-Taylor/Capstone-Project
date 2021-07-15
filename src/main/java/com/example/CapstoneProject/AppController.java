4c9b3b5c4b88afaf9152ae099a8643cf060018f5
Bryce-Taylor <btaylor21@basecampcodingacademy.org>
2021-07-15 16:23:39 -0500

    HomePage login styling update

--- src/main/java/com/example/CapstoneProject/AppController.java
+++ src/main/java/com/example/CapstoneProject/AppController.java
@@ -1,14 +1,17 @@
 package com.example.CapstoneProject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 public class AppController {
@@ -62,4 +65,6 @@ public class AppController {
         model.addAttribute("listUsers", listUsers);
         return "schedule";
     }
+
+
 }
